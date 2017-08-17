abbd5b834ae69afc7e80d64b684202d36de15b87
coreyfurman <cfurm14@gmail.com>
2017-08-16 22:00:50 -0400

    first push

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big \'n\' Tasty', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big \'n\' Nasty', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big \'n\' Crispy', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big \'n\' Delishy', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big \'n\' Yum', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big \'n\' Mmmm', true);
---
+++ models/customer.js
@@ -0,0 +1,20 @@
+// model for customer table... not used
+
+module.exports = function(sequelize, DataTypes) {
+    var Customer = sequelize.define("Customer", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        }
+    });
+    Customer.sync();
+    return Customer;
+};
